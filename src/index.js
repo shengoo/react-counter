ca257785fec12942e27a34b2b16f784f94e02ec1
Qing Sheng <shengoo@hotmail.com>
2019-05-09 11:23:56 +0800

    a
    a

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import registerServiceWorker from './registerServiceWorker';
-import FluxApp from './flux/FluxApp';
-import ReduxApp from './redux/ReduxApp'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import registerServiceWorker from "./registerServiceWorker";
+import FluxApp from "./flux/FluxApp";
+import ReduxApp from "./redux/ReduxApp";
 
-// ReactDOM.render(<FluxApp />, document.getElementById('root'));
-ReactDOM.render(<ReduxApp />, document.getElementById('root'));
+ReactDOM.render(<FluxApp />, document.getElementById("root"));
+// ReactDOM.render(<ReduxApp />, document.getElementById('root'));
 registerServiceWorker();
